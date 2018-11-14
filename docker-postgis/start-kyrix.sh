84f03e3f887398c2f12b4103803ca72d609fea4a
Adam Sah <140002+asah@users.noreply.github.com>
2018-11-14 02:53:34 +0000

    reduce noisy output, replace awkward re-run of nba.js

--- docker-postgis/start-kyrix.sh
+++ docker-postgis/start-kyrix.sh
@@ -9,24 +9,24 @@ psql postgresql://postgres:$POSTGRES_PASSWORD@db/postgres -c"CREATE DATABASE nba
 psql postgresql://kyrix:kyrix_password@db/kyrix -c "CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;CREATE EXTENSION fuzzystrmatch;CREATE EXTENSION postgis_tiger_geocoder;" || true
 psql postgresql://kyrix:kyrix_password@db/nba -c "CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;CREATE EXTENSION fuzzystrmatch;CREATE EXTENSION postgis_tiger_geocoder;" || true
 
+# workaround this issue: https://github.com/tracyhenry/Kyrix/issues/42
+psql postgresql://kyrix:kyrix_password@db/kyrix -c "CREATE TABLE IF NOT EXISTS project (name VARCHAR(255), content TEXT, dirty int, CONSTRAINT PK_project PRIMARY KEY (name));"
+psql postgresql://kyrix:kyrix_password@db/nba -c "CREATE TABLE IF NOT EXISTS project (name VARCHAR(255), content TEXT, dirty int, CONSTRAINT PK_project PRIMARY KEY (name));"
+
 echo "*** restoring NBA data to database..."
 cat nba_db_psql.sql | grep -v idle_in_transaction_session_timeout | psql postgresql://kyrix:kyrix_password@db/nba | egrep -i 'error' || true
 
-cd /kyrix/compiler/examples/nba
-echo "*** configuring for NBA examples - OK to ignore connection refused errors"
-node nba.js | egrep -i "error|connected" || true
-
 echo "*** starting tile server..."
 cd /kyrix/tile-server
-mvn exec:java -Dexec.mainClass="main.Main" | tee mvn-exec.out &
+mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn exec:java -Dexec.mainClass="main.Main" | stdbuf -oL grep -v Downloading: | tee mvn-exec.out &
 touch mvn-exec.out
-while [ -z "$(egrep 'Tile server started' mvn-exec.out)" ];do echo "waiting for tile server";sleep 5;done
+while [ -z "$(egrep 'Done precomputing|Tile server started' mvn-exec.out)" ];do echo "waiting for tile server";sleep 5;done
 
 echo "*** (re)configuring for NBA examples to ensure tile server recomputes..."
 cd /kyrix/compiler/examples/nba
 node nba.js | egrep -i "error|connected" || true
 
-echo "*** done! Kyrix is ready: visit http://<host>:8000/"
+echo "*** done! Kyrix ready at: http://<host>:8000/  (may need a minute to recompute indexes - watch this log for messages)"
 
 # keep mvn exec running in background, don't exit container
 tail -f /dev/null
