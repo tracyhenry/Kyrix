f61e853ae1f21c16fd4fc5a9e5fdabe6d4c6f8aa
Adam Sah <140002+asah@users.noreply.github.com>
2018-11-19 07:36:34 +0800

    auto-detect existing NBA data and avoid reload (#46)
    * auto-detect existing NBA data and avoid reload
    
    * oops, reverse logic to handle errors
    
    * correct fix, with better debug output
    
    patches begat patches... this still produces an ugly-looking error message, but at least it's functionally correct.

--- docker-scripts/start-kyrix.sh
+++ docker-scripts/start-kyrix.sh
@@ -13,8 +13,19 @@ psql postgresql://kyrix:kyrix_password@db/nba -c "CREATE EXTENSION postgis;CREAT
 #psql postgresql://kyrix:kyrix_password@db/kyrix -c "CREATE TABLE IF NOT EXISTS project (name VARCHAR(255), content TEXT, dirty int, CONSTRAINT PK_project PRIMARY KEY (name));"
 #psql postgresql://kyrix:kyrix_password@db/nba -c "CREATE TABLE IF NOT EXISTS project (name VARCHAR(255), content TEXT, dirty int, CONSTRAINT PK_project PRIMARY KEY (name));"
 
-echo "*** restoring NBA data to database..."
-cat nba_db_psql.sql | grep -v idle_in_transaction_session_timeout | psql postgresql://kyrix:kyrix_password@db/nba | egrep -i 'error' || true
+plays=$(psql postgresql://kyrix:kyrix_password@db/nba -X -P t -P format=unaligned -c "select count(*)>500000 from plays;" || true)
+if [ "$plays" = "t" ]; then
+    # if you want to force a reload, the easiest way is to dropdb, e.g.
+    #   docker exec -u postgres -it kyrix_db_1 sh -c "dropdb nba"
+    # note: requires the database to be running, i.e. let docker-compose finish starting up.
+    echo "NBA data found - skipping reload to avoid duplicate records."
+else
+    # TODO: prints ugly error message the first time
+    echo "NBA data not found - loading..."
+    cat nba_db_psql.sql | grep -v idle_in_transaction_session_timeout | psql postgresql://kyrix:kyrix_password@db/nba | egrep -i 'error' || true
+    numplays=$(psql postgresql://kyrix:kyrix_password@db/nba -X -P t -P format=unaligned -c "select count(*) from plays;" || true)
+    echo "numplays loaded: $numplays"
+fi
 
 echo "*** starting tile server..."
 cd /kyrix/tile-server
