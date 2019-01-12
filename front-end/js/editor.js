31673336c242ec194000eef6ce309d063a16a561
wenbo <thierryhenrytracy@163.com>
2019-01-11 21:15:55 -0500

    a simple login mechanism.

--- front-end/js/editor.js
+++ front-end/js/editor.js
@@ -14,17 +14,19 @@ $(function () {
 		var editBox = null;
 		var idBox = null;
 		var buttonsBox = null;
+		var passcodeBox = null;
 		var getIdFromRow = function () {
 			return row[0] + "_" + row[1] + "_" + row[2] + "_" + row[3];
 		};
 		var submitEdit = function () {
 			var label = $("input[name=label]:checked").val();
+			var passcode = $("input[name=passcode]").val();
 			$.ajax({
 				type: "POST",
 				url: "edit",
 				data: {
 					"item": getIdFromRow(),
-					"labeler": "mbw",
+					"labeler": passcode,
 					"label": labels[label]
 				},
 				success: function (data, status) {
@@ -49,13 +51,16 @@ $(function () {
 					option.attr('id', key);
 					option.on('change', submitEdit);
 					labelOptionDiv.append(option);
-//					labelOptionDiv.append($("<br>"));
 					var optionLabel = $("<label>");
 					optionLabel.attr("for", key);
 					optionLabel.text(labels[key]);
 					labelOptionDiv.append(optionLabel);
 					editBox.append(labelOptionDiv);
 				}
+				passcodeBox = $("<div id='passcodeBox'>");
+				passcodeBox.text("passcode: ");
+				passcodeBox.append($("<input name='passcode' type='text'>"));
+				editBox.append(passcodeBox);
 			}
 		};
 
@@ -63,7 +68,6 @@ $(function () {
 			floatBox = $("<div id='sidebar'>");
 			editBox = $("<div id='editor'>");
 			editBox.append($("<div id='editIdBox'>"));
-			editBox.append($("<div id='buttons'>"));
 			floatBox.append(editBox);
 			$("body").append(floatBox);
 			buttonsBox = $("#buttons");
