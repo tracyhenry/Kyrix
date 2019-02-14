0f41935272142625f9994306d751c6225c7f69bd
Adam Sah <140002+asah@users.noreply.github.com>
2019-02-14 10:01:53 -0500

    add docs

--- compiler/examples/nba/force-recompute.sh
+++ compiler/examples/nba/force-recompute.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# HACK!!!
+# force the server to reindex the data, e.g. for development purposes
+# the current method chosen is to hack the text of one of the SQL queries
+# in a harmless way that the Kyrix server can't detect but which the
+# underlying database will optimize-away.
+
 sed -i "s@1=1@`date +%s`>0@" transforms.js
 
 node nba.js
