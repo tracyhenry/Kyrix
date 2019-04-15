97a38701dcde1326bbd88b093a2ff67d835b55a6
Adam Sah <140002+asah@users.noreply.github.com>
2019-04-15 01:02:34 +0000

    reload-nba.sh: drop table first

--- compiler/examples/nba/reload-nba.sh
+++ compiler/examples/nba/reload-nba.sh
@@ -7,5 +7,5 @@ if [ ! -f nba_db_psql.sql ]; then
     wget -q -O nba_db_psql.sql 'https://www.dropbox.com/s/baqb01thxvfthk5/nba_db_psql.sql?dl=1' > /dev/null
 fi
 
-cat nba_db_psql.sql | grep -v idle_in_transaction_session_timeout
+perl -ne 'print if (s@^CREATE TABLE ([^ ]+).+@DROP TABLE IF EXISTS \1 CASCADE;@);' < nba_db_psql.sql | { cat; cat nba_db_psql.sql; } | egrep -v '^SET idle_in_transaction_session_timeout'
 
