b44379d2af3ddd3d724cc8c1e59f3ead96b091f2
Adam Sah <140002+asah@users.noreply.github.com>
2019-04-23 17:41:00 +0000

    reload-dots-uniform.sh: add citus_distribution_id

--- compiler/examples/dots-uniform/reload-dots-uniform.sh
+++ compiler/examples/dots-uniform/reload-dots-uniform.sh
@@ -6,9 +6,9 @@ if [ "x$PSQL" ]; then PSQL=`which psql`; fi
 if [ ! -x $PSQL ]; then echo "$0: $PSQL not found - consider setting PSQL to the psql(1) path."; exit 1; fi
 SCALE=${SCALE:-1}  # times 1M records
 
-$PSQL $PGCONN -t -c "drop table if exists dots_uniform cascade; create table dots_uniform(id int, w int, h int);"
+$PSQL $PGCONN -t -c "drop table if exists dots_uniform cascade; create table dots_uniform(id int, w int, h int, citus_distribution_id int);"
 
 for i in {1..100}; do
     echo `date +%s`": loading dots_uniform data #$i of 100..."
-    $PSQL $PGCONN -q -t -c "insert into dots_uniform (id,w,h) select id, (random()*1000000)::bigint, (random()*100000)::bigint from generate_series(1,10000*$SCALE) id;"
+    $PSQL $PGCONN -q -t -c "insert into dots_uniform (id,w,h, citus_distribution_id) select id, (random()*1000000)::bigint, (random()*100000)::bigint, (random()*2147483648*2.0 - 2147483648)::int from generate_series(1,10000*$SCALE) id;"
 done
