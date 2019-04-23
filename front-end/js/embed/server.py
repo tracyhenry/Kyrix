3ed10338883d1b9186c6ab5cf00f32e1133c440f
wenbo <thierryhenrytracy@163.com>
2019-04-23 17:21:55 -0400

    separate folder for embedding API related stuff.

---
+++ front-end/js/embed/server.py
@@ -0,0 +1,13 @@
+from bottle import route, run, static_file
+import os
+
+@route('/update', method = 'POST')
+def hello():
+    print "hello"
+    os.system('cd /home/ubuntu/Kyrix/front-end/js && git pull && cat embed/API.js globalVar.js jump.js parameter.js staticLayers.js zoom.js zoomButton.js pageOnLoad.js dynamicLayers.js > embed/all.js && cd embed/ && rollup -c')
+
+@route('/kyrix.js')
+def get():
+    return static_file('bundle.js', root='/home/ubuntu/Kyrix/front-end/js/embed/')
+
+run(host='128.52.164.24', port=8000, debug=True)
