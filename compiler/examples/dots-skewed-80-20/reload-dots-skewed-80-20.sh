9c0056ba75d46ba3d8cc8ac238981bdc358b1ea9
Adam Sah <140002+asah@users.noreply.github.com>
2019-04-23 17:39:18 +0000

    dots_skewed_80_20: add citus_distribution_id, which can be random and have dups

--- compiler/examples/dots-skewed-80-20/reload-dots-skewed-80-20.sh
+++ compiler/examples/dots-skewed-80-20/reload-dots-skewed-80-20.sh
@@ -6,14 +6,14 @@ if [ "x$PSQL" ]; then PSQL=`which psql`; fi
 if [ ! -x $PSQL ]; then echo "$0: $PSQL not found - consider setting PSQL to the psql(1) path."; exit 1; fi
 SCALE=${SCALE:-1}  # times 1M records
 
-$PSQL $PGCONN -t -c "drop table if exists dots_skewed_80_20 cascade; create table dots_skewed_80_20(id int, w int, h int);"
+$PSQL $PGCONN -t -c "drop table if exists dots_skewed_80_20 cascade; create table dots_skewed_80_20(id int, w int, h int, citus_distribution_id int);"
 
 for i in {1..80}; do
     echo `date +%s`": loading dots_skewed_80_20 data #$i of 100 (skewed to small area)..."
-    $PSQL $PGCONN -q -t -c "insert into dots_skewed_80_20 (id,w,h) select id, (random()*400000)::bigint, (random()*50000)::bigint from generate_series(1,10000*$SCALE) id;"
+    $PSQL $PGCONN -q -t -c "insert into dots_skewed_80_20 (id,w,h) select id, (random()*400000)::bigint, (random()*50000)::bigint, (random()*2147483648*2.0 - 2147483648)::int from generate_series(1,10000*$SCALE) id;"
 done
 
 for i in {81..100}; do
     echo `date +%s`": loading dots_skewed_80_20 data #$i of 100 (uniform)..."
-    $PSQL $PGCONN -q -t -c "insert into dots_skewed_80_20 (id,w,h) select id, (random()*1000000)::bigint, (random()*100000)::bigint from generate_series(1,10000*$SCALE) id;"
+    $PSQL $PGCONN -q -t -c "insert into dots_skewed_80_20 (id,w,h) select id, (random()*1000000)::bigint, (random()*100000)::bigint, (random()*2147483648*2.0 - 2147483648)::int from generate_series(1,10000*$SCALE) id;"
 done
