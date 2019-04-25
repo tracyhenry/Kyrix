1954b717aec00e96a9b76d785ba8ac50a41915c5
wenbo <thierryhenrytracy@163.com>
2019-04-25 10:35:37 -0400

    simpler rollup index.js & example app w/ oninput filtering.

--- front-end/js/embed/index.js
+++ front-end/js/embed/index.js
@@ -1,11 +1,1 @@
-//get kyrix svg
-export {initializeApp, filterData} from "./all";
-
-//trigger kyrix event
-export {triggerPan, triggerJump, highlightByInput, refresh} from "./all";
-
-//get static status
-export {getViews, getData, getSvg} from "./all";
-
-//capture pan/zoom event
-export {onPan, zoomControl} from "./all";
+export * from "./all";
