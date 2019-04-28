d832ec2c6e845c566860c4324687868b4e23994b
Adam Sah <140002+asah@users.noreply.github.com>
2019-04-28 17:02:08 +0000

    WIP: set shard count to 640 until we can autodetect

--- compiler/examples/dots-pushdown-uniform/reload-dots-pushdown-uniform.sh
+++ compiler/examples/dots-pushdown-uniform/reload-dots-pushdown-uniform.sh
@@ -6,16 +6,20 @@ if [ "x$PSQL" ]; then PSQL=`which psql`; fi
 if [ ! -x $PSQL ]; then echo "$0: $PSQL not found - consider setting PSQL to the psql(1) path."; exit 1; fi
 SCALE=${SCALE:-1}  # times 1M records
 
-echo "$0: PSQL = $PSQL"
-echo "$0: PGCONN = $PGCONN"
-cmd="drop table if exists dots_pushdown_uniform cascade; create table dots_pushdown_uniform(id bigint, w int, h int, citus_distribution_id int); select create_distributed_table('dots_pushdown_uniform', 'citus_distribution_id')"
+# TODO: auto-detect... $(python -c "print(max(32, $NUM_WORKERS * $NUM_CORES_PER_WORKER))")
+# (but this info isn't on the worker nodes...)
+SHARD_COUNT=640
+RECS=$(python -c "print(1000000*$SCALE)")
+       
+echo "$0: PSQL = $PSQL, PGCONN = $PGCONN - dropping and recreating dots_pushdown_uniform table with $RECS across $SHARD_COUNT shards."
+cmd="drop table if exists dots_pushdown_uniform cascade; create table dots_pushdown_uniform(id bigint, w int, h int, citus_distribution_id int); set citus.shard_count=$SHARD_COUNT; select create_distributed_table('dots_pushdown_uniform', 'citus_distribution_id')"
 echo "$cmd" | tee | $PSQL $PGCONN -q -t
 
 # TODO: divide by number of workers...
 for i in {1..10}; do
     echo `date +%s`": loading dots_pushdown_uniform data #$i of 10..."
     # careful to create unique IDs - use the shard id (right(<shard>,3))
-    cmd="select run_command_on_shards('dots_pushdown_uniform', \$\$ insert into %1\$s (id,w,h) select id * $i*1000 + right('%1\$s',3)::int, (random()*1000000)::bigint, (random()*100000)::bigint from generate_series(1,10000*$SCALE) id;\$\$)"
+    cmd="select run_command_on_shards('dots_pushdown_uniform', \$\$ insert into %1\$s (id,w,h) select (id::bigint) * $i*1000 + right('%1\$s',3)::bigint, (random()*1000000)::int, (random()*100000)::int from generate_series(1,(1000000*$SCALE/$SHARD_COUNT)::int) id;\$\$)"
     echo "$cmd" | tee | $PSQL $PGCONN -q -t
 done
 
