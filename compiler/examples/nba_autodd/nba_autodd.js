a717bee68595356e88c49c5397dbabd3d0d8d58a
wenbo <thierryhenrytracy@163.com>
2019-05-07 19:11:19 -0400

    order by aggregated ranking of the teams for the nba autodd game example.

--- compiler/examples/nba_autodd/nba_autodd.js
+++ compiler/examples/nba_autodd/nba_autodd.js
@@ -8,9 +8,10 @@ var p = new Project("nba_autodd", "../../../config.txt");
 p.addRenderingParams(renderers.renderingParams);
 
 // set up auto drill down
-var query = "select game_id, year, month, day, team1.abbr as home_team, team2.abbr as away_team, home_score, away_score "
+var query = "select game_id, year, month, day, team1.abbr as home_team, team2.abbr as away_team, home_score, away_score, team1.rank + team2.rank as agg_rank "
     + "from games, teams as team1, teams as team2 "
-    + "where games.home_team = team1.abbr and games.away_team = team2.abbr;";
+    + "where games.home_team = team1.abbr and games.away_team = team2.abbr "
+    + "order by agg_rank;";
 
 var args = {query : query, db : "nba",
     xCol : "home_score", yCol : "away_score",
