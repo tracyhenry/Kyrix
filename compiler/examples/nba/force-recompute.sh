6e962e3165f48492ae4def70ee996e4841905668
wenbo <thierryhenrytracy@163.com>
2019-06-14 22:46:43 -0400

    bug fixes.

--- compiler/examples/nba/force-recompute.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-FORCE=1 node nba.js
--- docker-scripts/install-d3.sh
+++ docker-scripts/install-d3.sh
@@ -18,4 +18,3 @@ echo "adding d3 to plv8-modules in database $1..."
 ./plv8-modules/bin/add_module_simple -d $1 --simple -n d3 --path /var/lib/postgresql/node_modules/d3/dist/d3.js
 echo "testing require('d3')..."
 echo "SET plv8.start_proc = 'commonjs.plv8_startup'; DO \$x\$ d3=require('d3');var d3_working=(d3.min([2,1,3])==1); if(d3_working) { plv8.elog(NOTICE, 'd3 working.') } else { plv8.elog(ERROR,'d3 not working'); }; \$x\$ LANGUAGE plv8; create function plv8_test_func() returns int as \$x\$ d3=require('d3'); return 42; \$x\$ language plv8;" | psql $1
-
