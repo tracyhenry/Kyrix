bc7987d4c66237676d25b388b43f1cce45d326ce
wenbo <thierryhenrytracy@163.com>
2019-08-05 18:59:25 -0400

    embed api for adding renderingparams.

--- front-end/js/embed/API.js
+++ front-end/js/embed/API.js
@@ -176,3 +176,9 @@ export function triggerJump(viewId, selector, layerId, jumpId) {
     // start jump
     startJump(viewId, curDatum, jump, optionalArgs);
 }
+
+export function addRenderingParameters(params) {
+    var keys = Object.keys(params);
+    for (var i = 0; i < keys.length; i++)
+        globalVar.renderingParmas[keys[i]] = params[keys[i]];
+}
