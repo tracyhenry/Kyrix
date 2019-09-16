078af87de1b8e95e8ce94765ff75b1fc27cf197f
wenbo <thierryhenrytracy@163.com>
2019-09-15 22:22:39 -0400

    temporary fallback for dots example
    because nativeboxindexer did not add params to transfunc.

--- compiler/examples/dots-pushdown-uniform/transforms.js
+++ compiler/examples/dots-pushdown-uniform/transforms.js
@@ -2,17 +2,17 @@ const Transform = require("../../src/Transform").Transform;
 
 var dotsTransform = new Transform({
     dbsource: "dots_pushdown_uniform",
-    transformFunc: function(id, w, h, params) {
+    transformFunc: function(id, w, h) {
         if (!("dots_pushdown_uniform_xscale" in plv8)) {
             // memoize for performance
             d3 = require("d3");
             plv8.dots_pushdown_uniform_xscale = d3
                 .scaleLinear()
-                .domain([0, params.topLevelWidth])
+                .domain([0, 100000])
                 .range([0, CANVAS_WIDTH]);
             plv8.dots_pushdown_uniform_yscale = d3
                 .scaleLinear()
-                .domain([0, params.topLevelHeight])
+                .domain([0, 100000])
                 .range([0, CANVAS_HEIGHT]);
         }
         // I'd use arrays for speed then annotate the column names, but plv8 can't return arrays
