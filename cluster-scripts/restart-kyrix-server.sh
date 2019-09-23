a70d567f9d8f3dc735a9e0e5346f4c2bad944adc
wenbo <thierryhenrytracy@163.com>
2019-09-23 14:13:11 -0400

    cleanup env files.

--- cluster-scripts/restart-kyrix-server.sh
+++ cluster-scripts/restart-kyrix-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source setup-kyrix-vars.env > /dev/null
+source env/setup-kyrix-vars.env > /dev/null
 source ../docker-scripts/spinner.sh
 
 DATA=${DATA:-nba}
@@ -47,7 +47,7 @@ fi
 
 # wait for external IP then export vars
 while [ 1 ]; do ip=`kubectl get services -o wide | grep kyrixserver | awk '{print $4}'`; if [ $ip != '<pending>' ]; then break; fi; spin "waiting for external IP"; done
-source setup-kyrix-vars.env > /dev/null
+source env/setup-kyrix-vars.env > /dev/null
 
 # note: DBTYPE=psql is safe, but won't distribute the data across the Citus cluster, i.e. it'll use local tables on the master Citus node only.
 CMD="cd /kyrix/back-end; KYRIX_IP=$KYRIX_IP KYRIX_DB=$KYRIX_DB SCALE=$SCALE NUM_WORKERS=$NUM_WORKERS SRCDATA_PROJECT_NAME=$SRCDATA_PROJECT_NAME SRCDATA_DB=$SRCDATA_DB SRCDATA_DB_TEST_TABLE=$SRCDATA_DB_TEST_TABLE SRCDATA_DB_LOAD_CMD=$SRCDATA_DB_LOAD_CMD KYRIX_DB_INDEX_CMD=$KYRIX_DB_INDEX_CMD KYRIX_DB_INDEX_FORCE=$KYRIX_DB_INDEX_FORCE KYRIX_DB_RELOAD_FORCE=$KYRIX_DB_RELOAD_FORCE DBTYPE=citus PGHOST=master POSTGRES_PASSWORD=kyrixftw USERNAME=kyrix USER_PASSWORD=kyrix_password /wait-for-postgres master:5432 -t 60 -- /start-kyrix.sh; tail -f /dev/null"
