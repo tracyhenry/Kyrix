fb3f04d8c20b8ef0cae6fb3e6d720ab41ffd9efe
wenbo <thierryhenrytracy@163.com>
2019-09-27 20:40:00 -0400

    multi-stage build dockerfile, ignoring postgis by default.

--- run-kyrix.sh
+++ run-kyrix.sh
@@ -3,6 +3,8 @@
 DB_PORT=5432
 KYRIX_PORT=8000
 START_APP=0
+BUILD_STAGE="pg-plv8"
+REBUILD=""
 
 while [[ $# -gt 0 ]]
 do
@@ -21,7 +23,15 @@ do
             KYRIX_PORT="$2"
             shift
             shift
-            ;; 
+            ;;
+        --postgis)
+            BUILD_STAGE="pg-plv8-postgis"
+            shift
+            ;;
+        --build)
+            REBUILD="--build"
+            shift
+            ;;
         *)
             echo "Wrong argument name $key"
             exit
@@ -29,4 +39,4 @@ do
     esac
 done
 
-DB_PORT=$DB_PORT KYRIX_PORT=$KYRIX_PORT START_APP=$START_APP docker-compose up
+START_APP=$START_APP DB_PORT=$DB_PORT KYRIX_PORT=$KYRIX_PORT BUILD_STAGE=$BUILD_STAGE docker-compose up $REBUILD
