a29d1faf5d63e122d5523e532fa3f1a592da9189
wenbo <thierryhenrytracy@163.com>
2019-09-29 16:15:09 -0400

    load data only if START_APP is 1.

--- docker-scripts/start-kyrix.sh
+++ docker-scripts/start-kyrix.sh
@@ -40,9 +40,6 @@ echo "*** setting up postgres roles/databases on master..."
 psql $PGCONN_STRING_POSTGRES/postgres -c "CREATE USER $USER_NAME WITH SUPERUSER PASSWORD '$USER_PASSWORD';" | egrep -v "$IGNORE_RX" 2>&1 || true
 # TOOD(citus): if kyrix is missing, we actually need to create on every node... currently, this is handled by redeploy-citus
 psql $PGCONN_STRING_POSTGRES/postgres -c "CREATE DATABASE kyrix OWNER $USER_NAME;" | egrep -v "$IGNORE_RX" 2>&1 || true
-# citus: this data isn't distributed, so these DBs
-psql $PGCONN_STRING_POSTGRES/postgres -c "CREATE DATABASE $SRCDATA_DB OWNER $USER_NAME;" | egrep -v "$IGNORE_RX" 2>&1 || true
-
 
 # if used, postgis is setup previously in the database - this is to support citus, which needs is initialized on every node
 # TODO: throwing errors: CREATE EXTENSION postgis_sfcgal; CREATE EXTENSION address_standardizer;CREATE EXTENSION address_standardizer_data_us;
@@ -51,26 +48,6 @@ psql $PGCONN_STRING_POSTGRES/postgres -c "CREATE DATABASE $SRCDATA_DB OWNER $USE
 #psql $PGCONN_STRING_USER/$SRCDATA_DB -c "$EXT_CMD" | egrep -v "$IGNORE_RX" 2>&1 || true
 
 cd /kyrix/back-end
-
-recs_exists=$(psql $PGCONN_STRING_USER/$SRCDATA_DB -X -P t -P format=unaligned -c "select exists(select 1 from information_schema.tables where table_schema='public' and table_name='$SRCDATA_DB_TEST_TABLE');")
-if [ "$recs_exists" = "t" ]; then
-    recs_found=$(psql $PGCONN_STRING_USER/$SRCDATA_DB -X -P t -P format=unaligned -c "select count(*)>$SRCDATA_DB_TEST_TABLE_MIN_RECS from $SRCDATA_DB_TEST_TABLE;")
-else
-    recs_found=f
-fi
-if [ "$recs_found" = "t" ] && [ "x$KYRIX_DB_RELOAD_FORCE" = "x0" ]; then
-    # if you want to force a reload, the easiest way is to dropdb, e.g.
-    #   docker exec -u postgres -it kyrix_db_1 sh -c "dropdb nba"
-    # note: requires the database to be running, i.e. let docker-compose finish starting up.
-    echo "raw data records found - skipping reload to avoid duplicate records."
-else
-    # TODO: prints ugly error message the first time
-    echo "raw data records not found - loading... PGCONN=$PGCONN_STRING_USER/$SRCDATA_DB loadcmd=$SRCDATA_DB_LOAD_CMD testing $SRCDATA_DB_TEST_TABLE"
-    PGCONN=$PGCONN_STRING_USER/$SRCDATA_DB $SRCDATA_DB_LOAD_CMD
-    numrecs=$(psql $PGCONN_STRING_USER/$SRCDATA_DB -X -P t -P format=unaligned -c "select count(*) from $SRCDATA_DB_TEST_TABLE;")
-    echo "raw data records loaded: $numrecs"
-    # TODO(asah): test for >SRCDATA_DB_TEST_TABLE_MIN_RECS
-fi
 
 while [ 1 ]; do KYRIX_PID=`ps awwwx | grep Slf4jMavenTransferListener | grep -v grep | head -1 | awk '{print $1}' | tr -d '\n'`; if [ "x$KYRIX_PID" == "x" ]; then break; fi; spin "backend server found - killing $KYRIX_PID..."; kill $KYRIX_PID; sleep 1; done
 
@@ -83,6 +60,29 @@ mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransfe
 while [ 1 ]; do if grep -E -q 'Done precomputing|Backend server started' mvn-exec.out; then break; fi; spin "waiting for backend server"; sleep 1; done
 
 if [ "x$START_APP" = "x1" ]; then
+    # citus: this data isn't distributed, so these DBs
+    psql $PGCONN_STRING_POSTGRES/postgres -c "CREATE DATABASE $SRCDATA_DB OWNER $USER_NAME;" | egrep -v "$IGNORE_RX" 2>&1 || true
+
+    recs_exists=$(psql $PGCONN_STRING_USER/$SRCDATA_DB -X -P t -P format=unaligned -c "select exists(select 1 from information_schema.tables where table_schema='public' and table_name='$SRCDATA_DB_TEST_TABLE');")
+    if [ "$recs_exists" = "t" ]; then
+        recs_found=$(psql $PGCONN_STRING_USER/$SRCDATA_DB -X -P t -P format=unaligned -c "select count(*)>$SRCDATA_DB_TEST_TABLE_MIN_RECS from $SRCDATA_DB_TEST_TABLE;")
+    else
+        recs_found=f
+    fi
+    if [ "$recs_found" = "t" ] && [ "x$KYRIX_DB_RELOAD_FORCE" = "x0" ]; then
+        # if you want to force a reload, the easiest way is to dropdb, e.g.
+        #   docker exec -u postgres -it kyrix_db_1 sh -c "dropdb nba"
+        # note: requires the database to be running, i.e. let docker-compose finish starting up.
+        echo "raw data records found - skipping reload to avoid duplicate records."
+    else
+        # TODO: prints ugly error message the first time
+        echo "raw data records not found - loading... PGCONN=$PGCONN_STRING_USER/$SRCDATA_DB loadcmd=$SRCDATA_DB_LOAD_CMD testing $SRCDATA_DB_TEST_TABLE"
+        PGCONN=$PGCONN_STRING_USER/$SRCDATA_DB $SRCDATA_DB_LOAD_CMD
+        numrecs=$(psql $PGCONN_STRING_USER/$SRCDATA_DB -X -P t -P format=unaligned -c "select count(*) from $SRCDATA_DB_TEST_TABLE;")
+        echo "raw data records loaded: $numrecs"
+        # TODO(asah): test for >SRCDATA_DB_TEST_TABLE_MIN_RECS
+    fi
+
     echo "*** (re)indexing (force=$KYRIX_DB_INDEX_FORCE)..."
     FORCE=$KYRIX_DB_INDEX_FORCE $KYRIX_DB_INDEX_CMD || true
 
