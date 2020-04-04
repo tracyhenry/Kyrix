ab232d22ca5cd5cb7ff374be5e5cd351aa4f5b25
tracyhenry <thierryhenrytracy@163.com>
2020-04-04 16:46:09 -0400

    superman transition (one way).

--- front-end/js/jump.js
+++ front-end/js/jump.js
@@ -453,29 +453,186 @@ function animateSlide(viewId, jump, predArray, newVpX, newVpY) {
         })
         .ease(d3.easeSinOut)
         .on("start", function() {
-            // schedule a new entering transition
-            d3.transition("enterTween_" + viewId)
-                .delay(param.slideSwitchDelay)
-                .duration(param.slideEnteringDuration)
-                .ease(d3.easeSinIn)
-                .tween("enterTween", function() {
+            // // new svg for rendering highway stuff
+            // var highwaySvg = d3.select(viewClass + ".oldlayerg")
+            //     .append("svg")
+            //     .attr("viewBox", "0 0 " + gvd.viewportWidth + " " + gvd.viewportHeight)
+            //     .attr("width", gvd.viewportWidth)
+            //     .attr("height", gvd.viewportHeight)
+            //     .style("opacity", 0)
+            //     .attr("id", "highwaysvg");
+            //
+            // // append the images
+            // var imgWidth = 318;
+            // var imgHeight = 240;
+            // var cx = gvd.viewportWidth / 2.0;
+            // var cy = gvd.viewportHeight / 2.0;
+            // var dx = imgHeight * cos;
+            // var dy = imgHeight * sin;
+            // for (var i = 0; i < 40; i ++) {
+            //     var j = Math.floor((i + 1) / 2) * (i % 2 == 0 ? 1 : -1);
+            //     var curX = cx + j * dx;
+            //     var curY = cy + j * dy;
+            //     highwaySvg.append("image")
+            //         .attr("x", curX - imgWidth / 2.0)
+            //         .attr("y", curY - imgHeight / 2.0)
+            //         .attr("width", imgWidth)
+            //         .attr("height", imgHeight)
+            //         .attr("xlink:href", "https://opengameart.org/sites/default/files/background-1_0.png")
+            //         .attr("transform", "rotate(" + (jump.slideDirection < 90 ?
+            //             90 - jump.slideDirection :
+            //             450 - jump.slideDirection)
+            //             + ", " + curX + ", " + curY + ")");
+            //
+            // }
+
+            // cloud svg
+            var cloudSvg = d3
+                .select(viewClass + ".oldlayerg")
+                .append("svg")
+                .attr(
+                    "viewBox",
+                    "0 0 " + gvd.viewportWidth + " " + gvd.viewportHeight
+                )
+                .attr("width", gvd.viewportWidth)
+                .attr("height", gvd.viewportHeight)
+                .style("opacity", 0)
+                .attr("id", "cloudsvg");
+
+            // append the images
+            var imgWidth = 256;
+            var imgHeight = 256;
+            var cx = gvd.viewportWidth / 2.0;
+            var cy = gvd.viewportHeight / 2.0;
+            var dx1 = Math.abs(imgHeight * cos * 1.5);
+            var dy1 = Math.abs(imgHeight * sin * 1.5);
+            var dx2 = Math.abs(imgWidth * sin * 1.5);
+            var dy2 = Math.abs(imgHeight * cos * 1.5);
+            for (var i = -10; i < 10; i++)
+                for (var j = -10; j < 10; j++) {
+                    var curX = cx + i * dx1 + i * dx2 + imgWidth;
+                    var curY = cy + j * dy1 + j * dy2 + imgHeight;
+                    console.log(curX + " " + curY);
+                    cloudSvg
+                        .append("image")
+                        .attr("x", curX - imgWidth / 2.0)
+                        .attr("y", curY - imgHeight / 2.0)
+                        .attr("width", imgWidth)
+                        .attr("height", imgHeight)
+                        .attr(
+                            "xlink:href",
+                            "https://live.staticflickr.com/65535/49735371613_70cb0051b2_b.jpg"
+                        )
+                        .attr(
+                            "transform",
+                            "rotate(" +
+                                (jump.slideDirection > 90 &&
+                                jump.slideDirection < 270
+                                    ? 360 - ((jump.slideDirection + 180) % 360)
+                                    : 360 - jump.slideDirection) +
+                                ", " +
+                                curX +
+                                ", " +
+                                curY +
+                                ")"
+                        );
+                }
+
+            var supermanSvg = d3
+                .select(viewClass + ".oldlayerg")
+                .append("svg")
+                .attr("id", "supermansvg")
+                .attr("width", gvd.viewportWidth)
+                .attr("height", gvd.viewportHeight)
+                .append("image")
+                .attr("x", gvd.viewportWidth / 2 - 150)
+                .attr("y", gvd.viewportHeight / 2 - 150)
+                .attr("width", 300)
+                .attr("height", 300)
+                .style("opacity", 0);
+            if (jump.slideDirection > 90 && jump.slideDirection < 270)
+                supermanSvg
+                    .attr(
+                        "xlink:href",
+                        "https://live.staticflickr.com/65535/49735899041_e6c9d13323_o.png"
+                    )
+                    .attr(
+                        "transform",
+                        "rotate(" +
+                            ((145 - jump.slideDirection + 360) % 360) +
+                            ", 500, 500)"
+                    );
+            else
+                supermanSvg
+                    .attr(
+                        "xlink:href",
+                        "https://live.staticflickr.com/65535/49735448721_e0ea4f763f_o.png"
+                    )
+                    .attr(
+                        "transform",
+                        "rotate(" +
+                            ((35 - jump.slideDirection + 360) % 360) +
+                            ", 500, 500)"
+                    );
+
+            d3.transition("cloudTween_" + viewId)
+                .duration(2500)
+                .ease(d3.easeLinear)
+                .tween("cloudTween", function() {
                     return function(t) {
-                        enter(t);
+                        travel(t);
                     };
                 })
                 .on("start", function() {
-                    // get the canvas object for the destination canvas
-                    var gotCanvas = getCurCanvas(viewId);
-                    gotCanvas.then(function() {
-                        // static trim
-                        renderStaticLayers(viewId);
+                    // highwaySvg.transition().duration(600).style("opacity", 1);
+                    supermanSvg
+                        .transition()
+                        .delay(200)
+                        .duration(300)
+                        .style("opacity", 1);
 
-                        // render
-                        RefreshDynamicLayers(viewId, newVpX, newVpY);
-                    });
+                    cloudSvg
+                        .transition()
+                        .delay(200)
+                        .duration(300)
+                        .style("opacity", 1);
+
+                    // supermanSvg.transition().duration(100).style("opacity", 1);
+                    // cloudSvg.transition().duration(100).style("opacity", 1);
+                    // schedule a new entering transition
+                    d3.transition("enterTween_" + viewId)
+                        .delay(2000)
+                        .duration(param.slideEnteringDuration)
+                        .ease(d3.easeSinIn)
+                        .tween("enterTween", function() {
+                            return function(t) {
+                                enter(t);
+                            };
+                        })
+                        .on("start", function() {
+                            cloudSvg
+                                .transition()
+                                .duration(400)
+                                .style("opacity", 0);
+
+                            // get the canvas object for the destination canvas
+                            var gotCanvas = getCurCanvas(viewId);
+                            gotCanvas.then(function() {
+                                // static trim
+                                renderStaticLayers(viewId);
+
+                                // render
+                                RefreshDynamicLayers(viewId, newVpX, newVpY);
+                            });
+                        })
+                        .on("end", function() {
+                            postJump(viewId, jumpType);
+                        });
                 })
                 .on("end", function() {
-                    postJump(viewId, jumpType);
+                    // highwaySvg.remove();
+                    supermanSvg.remove();
+                    cloudSvg.remove();
                 });
         });
 
@@ -549,6 +706,28 @@ function animateSlide(viewId, jump, predArray, newVpX, newVpY) {
             minx = ((-gvd.viewportHeight * (1 - t)) / Math.abs(sin)) * cos;
         }
         d3.selectAll(viewClass + ".mainsvg.static").attr(
+            "viewBox",
+            minx +
+                " " +
+                miny +
+                " " +
+                gvd.viewportWidth +
+                " " +
+                gvd.viewportHeight
+        );
+    }
+
+    function travel(t) {
+        // change viewbox of static layers
+        if (Math.abs(cos) > Math.abs(sin)) {
+            minx = -gvd.viewportWidth * (1 - 2 * t) * (cos > 0 ? 1 : -1);
+            miny = ((-gvd.viewportWidth * (1 - 2 * t)) / Math.abs(cos)) * sin;
+        } else {
+            miny = -gvd.viewportHeight * (1 - 2 * t) * (sin > 0 ? 1 : -1);
+            minx = ((-gvd.viewportHeight * (1 - 2 * t)) / Math.abs(sin)) * cos;
+        }
+        console.log("haha : " + minx + " " + miny);
+        d3.select("#cloudsvg").attr(
             "viewBox",
             minx +
                 " " +
