a3e5562f908f8707b18609361cd882cc88eb6f98
tracyhenry <thierryhenrytracy@163.com>
2020-05-28 16:36:11 -0700

    one more try.

--- back-end/src/main/java/server/IndexHandler.java
+++ back-end/src/main/java/server/IndexHandler.java
@@ -26,7 +26,7 @@ public class IndexHandler implements HttpHandler {
 
             String path = httpExchange.getRequestURI().getPath();
             if (path.contains("favicon.ico")) {
-                Server.sendResponse(httpExchange, HttpsURLConnection.HTTP_NO_CONTENT, "no content");
+                Server.sendResponse(httpExchange, HttpsURLConnection.HTTP_BAD_REQUEST, "");
                 return;
             }
             if (path.equals("/")) path = "/" + Config.indexFileName;
