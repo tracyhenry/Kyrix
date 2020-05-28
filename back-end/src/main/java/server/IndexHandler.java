0daa92212f15d8e4565c243a21c4f161b915a62e
tracyhenry <thierryhenrytracy@163.com>
2020-05-28 16:22:19 -0700

    try to ignore favicon.ico

--- back-end/src/main/java/server/IndexHandler.java
+++ back-end/src/main/java/server/IndexHandler.java
@@ -25,6 +25,10 @@ public class IndexHandler implements HttpHandler {
             }
 
             String path = httpExchange.getRequestURI().getPath();
+            if (path.contains("favicon.ico")) {
+                Server.sendResponse(httpExchange, HttpsURLConnection.HTTP_NO_CONTENT, "");
+                return;
+            }
             if (path.equals("/")) path = "/" + Config.indexFileName;
 
             // read the frontend file and return
