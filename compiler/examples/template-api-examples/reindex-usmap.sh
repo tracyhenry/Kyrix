93d2612017f954030e1f132d7cbcd0465dd6ab0e
tracyhenry <thierryhenrytracy@163.com>
2020-10-11 17:42:18 -0700

    remove useless files.

--- compiler/examples/template-api-examples/reindex-usmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# wrapped as a script for start-kyrix.sh
-
-#cd /kyrix/compiler/examples/template-api-examples
-cd /kyrix/compiler/examples/USMap/
-node USMap.js | egrep -i "error|connected" || true
