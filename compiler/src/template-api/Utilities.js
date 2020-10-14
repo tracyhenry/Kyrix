74afcc2151c76deb057c22101e8b96d3070591ac
tracyhenry <thierryhenrytracy@163.com>
2020-10-14 09:30:18 -0700

    no checkArgs necessary, switching to json validator.

--- compiler/src/template-api/Utilities.js
+++ compiler/src/template-api/Utilities.js
@@ -194,35 +194,6 @@ function serializePath(path) {
     }, "");
 }
 
-function checkArgs(objName, requiredArgs, requiredArgTypes, args) {
-    // check required args
-    for (var i = 0; i < requiredArgs.length; i++) {
-        if (!(requiredArgs[i] in args))
-            throw new Error(
-                "Constructing " + objName + ": " + requiredArgs[i] + " missing."
-            );
-        if (typeof args[requiredArgs[i]] !== requiredArgTypes[i])
-            throw new Error(
-                "Constructing " +
-                    objName +
-                    ": " +
-                    requiredArgs[i] +
-                    " must be " +
-                    requiredArgTypes[i] +
-                    "."
-            );
-        if (requiredArgTypes[i] == "string")
-            if (args[requiredArgs[i]].length == 0)
-                throw new Error(
-                    "Constructing " +
-                        objName +
-                        ": " +
-                        requiredArgs[i] +
-                        " cannot be an empty string."
-                );
-    }
-}
-
 module.exports = {
     textwrap,
     getBodyStringOfFunction,
