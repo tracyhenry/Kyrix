e46b6da2ca0b0bf6c0960afcc25a8c6994251ee1
tracyhenry <thierryhenrytracy@163.com>
2020-11-21 23:25:29 -0800

    trim

--- compiler/src/template-api/Pie.js
+++ compiler/src/template-api/Pie.js
@@ -52,42 +52,22 @@ function Pie(args) {
     var transform = new Transform(query, args.db, transform_func, schema, true);
 
     // Pie is a dynamic layer
-    Layer.call(this, transform, false);
+    Layer.call(this, transform, true);
 
-    this.cx = args.cx || 300;
-    this.cy = args.cy || 300;
     this.innerRadius = args.innerRadius || 0.01;
     this.outerRadius = args.outerRadius || 200;
 
     this.renderingParams = {
         [pie_name]: {
-            cx: this.cx,
-            cy: this.cy,
             innerRadius: this.innerRadius,
             outerRadius: this.outerRadius,
             colorInterpolator: args.colorInterpolator || "Viridis",
-            labelShow: args.labelShow || false,
-            labelRadius: args.labelRadius || -1,
-            valueShow: args.valueShow || false,
-            valueRadius: args.valueRadius || -1,
-            percentageShow: args.percentageShow || false,
-            percentageRadius: args.percentageRadius || -1,
-            panelPercentage: args.panelPercentage || -1,
             cornerRadius: args.cornerRadius || 0,
             padAngle: args.padAngle || 0,
             transitions: args.transitions || [],
             indices: args.indices || []
         }
     };
-
-    var piePlacement = {
-        centroid_x: "con:" + this.cx,
-        centroid_y: "con:" + this.cy,
-        width: "con:" + this.outerRadius,
-        height: "con:" + this.outerRadius
-    };
-
-    this.addPlacement(piePlacement);
 
     var rendering_func = getPieRenderer(pie_name);
 
@@ -179,17 +159,7 @@ function getPieRenderer(pie_name) {
         });
 
         var cooked = pie(data);
-        // console.log("cooked: ", cooked);
-
-        var scalePercent = d3
-            .scaleLinear()
-            .domain([0, 2 * Math.PI])
-            .range([0, 1]);
-        var formatter = d3.format(".1%");
         cooked.forEach(entry => {
-            entry.percentage = formatter(
-                scalePercent(entry.endAngle - entry.startAngle)
-            );
             for (x in entry.data) {
                 entry[x] = entry.data[x];
             }
@@ -198,10 +168,6 @@ function getPieRenderer(pie_name) {
 
         var g = svg.append("g").attr("class", "gPie");
 
-        g.attr("transform", function() {
-            return "translate(" + params.cx + ", " + params.cy + " ) ";
-        });
-
         var arc = d3
             .arc()
             .innerRadius(params.innerRadius)
@@ -209,18 +175,11 @@ function getPieRenderer(pie_name) {
             .cornerRadius(params.cornerRadius)
             .padAngle(params.padAngle);
 
-        var x = Number(params.x) || 0;
-        var y = Number(params.y) || 0;
-
         var slices = g
             .selectAll("path.value")
             .data(cooked)
             .enter()
-            .append("g")
-            .attr("class", function(d, i) {
-                return "slice slice" + i;
-            });
-
+            .append("g");
         slices
             .append("path")
             .attr("class", function(d, i) {
@@ -232,127 +191,16 @@ function getPieRenderer(pie_name) {
                 return ret;
             });
 
-        mark("label");
-        mark("value");
-        mark("percentage");
-
-        if (params.percentageShow || params.labelShow || params.valueShow) {
-            slices
-                .filter(d => {
-                    return (
-                        Number(d.percentage.replace("%", "")) <=
-                        params.panelPercentage
-                    );
-                })
-                .on("mouseover", function(d, i) {
-                    d3.selectAll(".tooltip").remove();
-                    var tooltip = d3
-                        .select("body")
-                        .append("div")
-                        .attr("id", "tooltip" + i)
-                        .classed("tooltip panel panel-info", true)
-                        .style("opacity", 0)
-                        .style("left", d3.event.pageX + 10 + "px")
-                        .style("top", d3.event.pageY + 50 + "px");
-
-                    if (params.labelShow) {
-                        tooltip
-                            .append("div")
-                            .attr("class", "panel-heading")
-                            .append("h3")
-                            .attr("class", "panel-title")
-                            .text(d.label);
-                    }
-                    if (params.valueShow || params.percentageShow) {
-                        var table = tooltip
-                            .append("table")
-                            .classed("table table-sm", true);
-                        var thead = table.append("thead").append("tr");
-                        var line = table.append("tbody").append("tr");
-                        if (params.valueShow) {
-                            thead.append("th").text("Value");
-                            line.append("td").text(d.value);
-                        }
-                        if (params.percentageShow) {
-                            thead.append("th").text("Percentage");
-                            line.append("td").text(d.percentage);
-                        }
-                    }
-                    tooltip
-                        .transition()
-                        .duration(1000)
-                        .style("opacity", 0.9)
-                        .style("top", d3.event.pageY + 10 + "px");
-                })
-                .on(
-                    "click.tooltip",
-                    function(d, i) {
-                        d3.selectAll(".tooltip").remove();
-                    },
-                    true
-                )
-                .on("mouseout", function(d, i) {
-                    d3.selectAll(".tooltip").remove();
-                });
-        }
-
         var random = d3.randomUniform(0, data.length);
-        var trs = [];
-        var trs_onload = [];
-        var flag_slow_in = false;
-        var flag_on_load = false;
-        var flag_hover_load = false;
-
         for (var i = params.transitions.length - 1; i >= 0; i--) {
-            if (params.transitions[i].name == "color shift") {
-                var tri = trs.push(preprocess(params.transitions[i])) - 1;
-                colorShift(trs[tri]);
-            } else if (params.transitions[i].name == "slow in") {
-                // slow in can only be excecuted once
-                if (!flag_slow_in) {
-                    var tri = trs.push(preprocess(params.transitions[i])) - 1;
-                    slowIn(trs[tri]);
-                    flag_slow_in = true;
-                }
-            } else if (params.transitions[i].name == "radius shift") {
-                var tri = trs.push(preprocess(params.transitions[i])) - 1;
-                radiusShift(trs[tri]);
-            } else if (params.transitions[i].name == "pizza hover") {
-                var tri = trs.push(preprocess(params.transitions[i])) - 1;
-                trs_onload.push(trs[tri]);
-            }
-        }
-
-        function mark(type) {
-            if (params[type + "Show"] !== true) return;
-
-            if (params[type + "Radius"] > 0) {
-                var arcTemp = arc
-                    .innerRadius(params[type + "Radius"])
-                    .outerRadius(params[type + "Radius"]);
-            } else {
-                var arcTemp = arc;
-            }
-            // console.log("params."+type+"Radius: ", params[type + "Radius"]);
-            slices
-                .filter(d => {
-                    return (
-                        Number(d.percentage.replace("%", "")) >
-                        params.panelPercentage
-                    );
-                })
-                .append("text")
-                .attr("class", "font-weight-normal")
-                .attr("transform", function(d) {
-                    return "translate(" + arcTemp.centroid(d) + ")";
-                })
-                .text(function(d) {
-                    return d[type];
-                });
+            if (params.transitions[i].name == "slow in")
+                slowIn(preProcess(params.transitions[i]));
+            else if (params.transitions[i].name == "radius shift")
+                radiusShift(preProcess(params.transitions[i]));
         }
 
         // tr for transition
-        function preprocess(args) {
+        function preProcess(args) {
             var type =
                 args.name == "slow in"
                     ? args.type || "rotate"
@@ -372,86 +220,11 @@ function getPieRenderer(pie_name) {
             };
         }
 
-        function colorShift(tr) {
-            // console.log("Var Color Detected", tr);
-            var interpolators = [
-                // These are from d3-scale.
-                "Viridis",
-                "Inferno",
-                "Magma",
-                "Plasma",
-                "Warm",
-                "Cool",
-                "Rainbow",
-                // "CubehelixDefault",
-                // These are from d3-scale-chromatic
-                // "Blues",
-                // "Greens",
-                // "Greys",
-                // "Oranges",
-                // "Purples",
-                // "Reds",
-                "BuGn",
-                "BuPu",
-                "GnBu",
-                "OrRd",
-                "PuBuGn",
-                "PuBu",
-                "PuRd",
-                "RdPu",
-                "YlGnBu",
-                "YlGn",
-                "YlOrBr",
-                "YlOrRd"
-            ];
-            var index = 0;
-            var count = 0;
-            var colorCb = function(index) {
-                var curColor = interpolators[index % interpolators.length];
-                // console.log("curInterpolator:", curColor);
-                var curInterpolator = d3["interpolate" + curColor];
-                color.interpolator(curInterpolator);
-                g.selectAll("path.value")
-                    .transition()
-                    .delay((d, i, nodes) => {
-                        if (tr.order == "desc") {
-                            return d.index * tr.gap + tr.delay;
-                        } else if (tr.order == "asc") {
-                            return (
-                                (nodes.length - d.index - 1) * tr.gap + tr.delay
-                            );
-                        } else if (tr.order == "random") {
-                            return random() * tr.gap + tr.delay;
-                        } else {
-                            throw new Error("unsupported order");
-                        }
-                    })
-                    .duration(tr.duration)
-                    .ease(d3["ease" + tr.ease])
-                    .attr("fill", function(d, i) {
-                        // d = d. data mark
-                        return color(d.value);
-                        // return color(colorScale(d.value));
-                    });
-            };
-            var interval = d3.interval(function() {
-                index = Math.floor(d3.randomUniform(0, interpolators.length)());
-                // index++;
-                colorCb(index % interpolators.length);
-                count++;
-                if (tr.end > 0 && count > tr.end) {
-                    interval.stop();
-                }
-            }, tr.period);
-        }
-
         function slowIn(tr) {
-            // console.log("slow in detected:",tr, " with type: ", tr.type);
             var arc2degree = d3
                 .scaleLinear()
                 .domain([0, 2 * Math.PI])
                 .range([0, 360]);
-            // invisible in the beginning
             slices.style("opacity", 0);
 
             if (tr.type == "rotate") {
@@ -470,22 +243,18 @@ function getPieRenderer(pie_name) {
                             );
                         } else if (tr.order == "random") {
                             return random() * tr.gap + tr.delay;
-                        } else {
-                            throw new Error("unsupported order");
                         }
                     })
                     .duration(tr.duration)
                     .ease(d3["ease" + tr.ease])
                     .attrTween("transform", (d, i) => {
-                        // d = d. data mark
-                        // console.log("attrTween!", d.label, d.startAngle);
                         return d3.interpolate(
                             "rotate(-" + arc2degree(d.startAngle) + ")",
                             "rotate(0)"
                         );
                     })
                     .style("opacity", 1)
-                    .on("end", transitions_onload);
+                    .on("end", setPizzaHover);
             } else if (tr.type == "translate") {
                 var tr_f = function(pos) {
                     var ret = "translate(";
@@ -522,18 +291,13 @@ function getPieRenderer(pie_name) {
                         return tr_f("end");
                     })
                     .style("opacity", 1)
-                    .on("end", transitions_onload);
+                    .on("end", setPizzaHover);
             }
         }
 
         function radiusShift(tr) {
-            // console.log("radius shift detected, with type: ", tr.type);
-            if (tr.start == "0" || tr.start == 0) {
-                tr.start = 0.1;
-            }
-            if (tr.end == "0" || tr.end == 0) {
-                tr.end = 0.1;
-            }
+            tr.start = 0.1;
+            tr.end = 0.1;
 
             if (tr.type == "inner") {
                 var arc0 = arc
@@ -556,8 +320,6 @@ function getPieRenderer(pie_name) {
                         return (nodes.length - d.index - 1) * tr.gap + tr.delay;
                     } else if (tr.order == "random") {
                         return random() * tr.gap + tr.delay;
-                    } else {
-                        throw new Error("unsupported order");
                     }
                 })
                 .duration(tr.duration)
@@ -576,10 +338,9 @@ function getPieRenderer(pie_name) {
                 });
         }
 
-        function pizzaHover(tr) {
-            // console.log("pizza hover detected:", tr);
+        function setPizzaHover() {
             slices
-                .on("mouseover.pizza", function(d, i, nodes) {
+                .on("mouseover.pizza", function(d) {
                     d3.select(this)
                         .filter(d => {
                             return !d.pizzaFlag;
@@ -588,18 +349,16 @@ function getPieRenderer(pie_name) {
                         .delay(tr.delay)
                         .duration(tr.duration)
                         .ease(d3["ease" + tr.ease])
-                        .attr("transform", function(d, i, nodes) {
+                        .attr("transform", function(d) {
                             d.pizzaFlag = true;
                             return (
                                 "translate(" +
-                                arc.centroid(d).map(item => {
-                                    return item * tr.end;
-                                }) +
+                                arc.centroid(d).map(item => item * tr.end) +
                                 ")"
                             );
                         });
                 })
-                .on("mouseout.pizza", function(d, i, nodes) {
+                .on("mouseout.pizza", function(d) {
                     d3.select(this)
                         .filter(d => {
                             return d.pizzaFlag;
@@ -613,17 +372,6 @@ function getPieRenderer(pie_name) {
                             return "";
                         });
                 });
-        }
-
-        function transitions_onload() {
-            if (flag_on_load) return;
-            for (var i = trs_onload.length - 1; i >= 0; i--) {
-                if (trs_onload[i].name == "pizza hover" && !flag_hover_load) {
-                    pizzaHover(trs_onload[i]);
-                    flag_hover_load = true;
-                }
-            }
-            flag_on_load = true;
         }
     }
 }
