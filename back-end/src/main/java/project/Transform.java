5c4863ba7700f68a7fa8af5610ab1b932de67f52
Peter Griggs <pgriggs@mit.edu>
2021-04-20 20:58:10 -0400

    modified Transform object in Transform.java and Transform.js to implement reverseFunctions

--- back-end/src/main/java/project/Transform.java
+++ back-end/src/main/java/project/Transform.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Map;
 import main.Config;
 import main.DbConnector;
 
@@ -19,6 +20,7 @@ public class Transform implements Serializable {
     private String transformFuncBody;
     private ArrayList<String> columnNames, queriedColumnNames = null;
     private ArrayList<String> filterableColumnNames;
+    private Map<String, String> reverseFunctions;
     private boolean separable;
 
     public String getId() {
